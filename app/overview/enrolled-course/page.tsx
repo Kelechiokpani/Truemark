032982115c18cda616f513bd269263bb5044d1b6
emmanuel.okpani <mremmatex@gmail.com>
2025-09-26 02:10:04 +0100

    update course and erolled

---
+++ app/overview/enrolled-course/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import CourseListing from "@/components/dashboard/Course/CourseListing";
+
+export default function OverviewPage() {
+  return (
+    <div className="min-h-screen bg-white">
+      <CourseListing />
+    </div>
+  );
+}
---
+++ components/dashboard/EnrolledCourse/Enrolled-Course-Item.tsx
@@ -0,0 +1,69 @@
+"use client";
+import Image from "next/image";
+import { CourseList } from "@/types/blog";
+import { useCourseStore } from "@/store/useCourseStore";
+import Link from "next/link";
+import React from "react";
+
+
+const CourseItem = ({ courseListing, paid }: { courseListing: CourseList, paid:boolean }) => {
+  const {image, name, price, id } = courseListing;
+
+  const setSelectedCourse = useCourseStore((s) => s.setSelectedCourse);
+   const {addToCart, } = useCourseStore()
+
+  const handleClick = () => {
+    setSelectedCourse(courseListing);
+  };
+
+   const paidUrl = `/overview/course/${courseListing.id}`
+   const url = `/overview/course/${courseListing.id}`
+
+  // `/overview/course/${courseListing.id}`
+  return (
+    <div className="border rounded-lg overflow-hidden shadow-sm hover:shadow-md transition cursor-pointer">
+      <div className="relative">
+        <Link
+          href={paid ? paidUrl : url}
+          // href={`/overview/course/${courseListing.id}`}
+          onClick={handleClick}
+        >
+          <Image
+            src={image}
+            alt={name}
+            className="w-full h-40 object-cover"
+            width={500}
+            height={160}
+          />
+        </Link>
+      </div>
+
+      <div className="p-4 flex justify-between">
+        <h3 className="font-semibold ">{name}</h3>
+      </div>
+
+      <p className="text-lg text-gray-600 px-5 ">
+        {new Intl.NumberFormat("en-NG", {
+          style: "currency",
+          currency: "NGN",
+        }).format(Number(price ?? 0))}
+      </p>
+
+
+       <div className="flex flex-col gap-4 py-2 px-4">
+        <button
+          onClick={() => addToCart(courseListing)}
+          className="bg-[#387467] text-white px-4 py-2 rounded-lg"
+        >
+          Add to Cart
+           {/*{isPaid ? "Already Enrolled" : "Add to Cart"}*/}
+        </button>
+      </div>
+
+
+    </div>
+  )
+
+};
+
+export default CourseItem;
